cb2190e329cd918219255fd44d4a368ac81ecb91
matheusbronzel@hotmail.com <matheusbronzel@hotmail.com>
2026-05-26 17:48:56 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Background implementation: parallax

--- code/menu.py
+++ code/menu.py
@@ -4,7 +4,7 @@ import pygame as pg
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -16,25 +16,38 @@ class Menu:
                                        top=0)  # o retangulo já inicia nas coordenadas 0,0, porem especifiquei apenas para entendimento
 
     def run(self):
-
+        menu_option = 0
         pg.mixer.music.load("./asset/Menu.mp3")
         pg.mixer.music.play(-1)  # o parametro -1 serve para a musica ficar em loop
         while True:
+            # Draw images
             self.window.blit(source=self.surf, dest=self.rect)  # pegando a imagem e jogando no retangulo
             self.menu_text(50, "Moutain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 110))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pg.display.flip()  # atualizando a tela
+
 
             # check for all events
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     quit()  # end pygame
-                    return
+
+                if event.type == pg.KEYDOWN: # all keys press events
+                    if event.key in (pg.K_s, pg.K_DOWN): # event of scrolling the menu
+                        menu_option = (menu_option + 1) % len(MENU_OPTION)
+
+                    if event.key in (pg.K_w, pg.K_UP): # event to rise the menu
+                        menu_option = (menu_option - 1) % len(MENU_OPTION)
+
+                    if event.key == pg.K_RETURN:  # press ENTER
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pg.font.SysFont("Lucida Sans Typewriter", text_size)
